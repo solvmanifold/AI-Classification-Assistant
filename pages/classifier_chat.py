594402e45eb157c57cd1fef63d97fa848df0f4cd
Mogith PN <mogith.nirmala@clarifai.com>
2024-04-15 18:19:34 +0000

    Added few shot and zero shot classification features

--- pages/classifier_chat.py
+++ pages/classifier_chat.py
@@ -1,9 +1,11 @@
+import os
 import streamlit as st
 from streamlit_option_menu import option_menu
 from utils import *
 import pandas as pd
 from clarifai.modules.css import ClarifaiStreamlitCSS
 from langchain_community.llms import Clarifai as ClarifaiLLM
+from langchain.vectorstores import Clarifai as clarifaivectorstore
 
 st.set_page_config(layout="wide")
 ClarifaiStreamlitCSS.insert_default_css(st)
@@ -32,58 +34,88 @@ if 'start_chat' not in st.session_state.keys():
     
 if "chat_history" not in st.session_state.keys():
   st.session_state['chat_history'] = [{"Query": "Let's classify", "content": "How may I help you?"}]
+      
+def init_db(no_of_examples):
+  query_params = st.experimental_get_query_params()
+  USER_ID = query_params["user_id"][0] if "user_id" in query_params.keys() else None
+  APP_ID = query_params["app_id"][0] if "app_id" in query_params.keys() else None
+  vectorDB = clarifaivectorstore(
+    user_id=USER_ID,
+    app_id=APP_ID,
+    number_of_docs=no_of_examples*3,
+    pat = PAT
+  )
+  return vectorDB
 
-with st.sidebar:
-  PAT = st.text_input("Enter your Clarifai PAT", type="password", key="PAT")
-  model_url = st.text_input("Enter Clarifai LLM model URL", key="model_url")
-  if model_url:
-    llm = ClarifaiLLM(model_url=model_url, pat=PAT)
-    if "config" not in st.session_state.keys():
-      st.session_state["config"] = True
+def retrieve_and_parse_few_shot(user_query, vectorDB):
+  examples = few_shot_examples(vectorDB, user_query)
+  return few_shot_promt_template(examples,user_query)
 
+def zero_shot_examples():
+  with open("../module-classifier/ODNI_examples.html") as f:
+    examples = f.read()
+    return examples
 
-def chatbot():
-  """chatbot """
-  if message := st.chat_input(key="input"):
-    st.chat_message("user").write(message)
-    st.session_state['chat_history'].append({"role": "user", "content": message})
-    with st.chat_message("assistant"):
-      with st.spinner("Thinking..."):
-        PT = prompt_template(st.session_state['few_shot_examples'], message)
-        response = llm(PT)
-        st.write(response)
-        message = {"role": "assistant", "content": response}
-        st.session_state['chat_history'].append(message)
-        
 def selectbox():
     with st.sidebar:
-        counter1 = option_menu("previous queries", [st.session_state['chat_history'][-1],st.session_state['chat_history'][-2]],icons=['chat', 'chat'], menu_icon="cast", default_index=0)
-        if counter1:
-            st.write(str(counter1))
-            
-def textbox():
+        counter1 = option_menu("previous queries", 
+                               [st.session_state['chat_history'][-1],st.session_state['chat_history'][-2]],
+                               icons=['chat', 'chat'], menu_icon="cast", default_index=0)
+
+def model_Select():
+  llm_model,llm_params = llm_models()
+  selected_model = st.selectbox("Select the LLM model", llm_model)
+  if selected_model:
+    llm = ClarifaiLLM(model_url=llm_params[selected_model], pat=PAT)
+  return llm
+ 
+def textbox(llm, mode, zero_shot_examples, no_of_examples : int = 1):
    user_query = st.text_area("Enter your text here", key="text_area")
-   if user_query:
+   classify_button = st.button("Classify", key="classify_btn") 
+   if classify_button: 
     with st.spinner("Thinking..."):
-          PT = prompt_template(st.session_state['few_shot_examples'], user_query)
-          response = llm(PT)
-          st.write(response)
-          message = [{"Query": user_query, "Response": response}]
-          st.session_state['chat_history'].append(message)
-          if len(st.session_state['chat_history']) >= 2:
-            selectbox()
+      if mode == "Few shot":
+        vectorDB = init_db(no_of_examples)
+        prompt = retrieve_and_parse_few_shot(user_query, vectorDB)
+      else:
+        prompt = prompt_template(zero_shot_examples, user_query)
+        
+      response = llm(prompt)
+      st.write(response)
+      message = [{"Query": user_query, "Response": response}]
+      st.session_state['chat_history'].append(message)
+      if len(st.session_state['chat_history']) >= 2:
+        selectbox()
         
 if not st.session_state['start_chat']:
     chatbtn = st.button("Start Chatting", key="chat_btn")
     if chatbtn:
        st.session_state['start_chat'] = True
        st.experimental_rerun()
+       
+with st.sidebar:
+  PAT = st.text_input("**Enter your Clarifai PAT**", type="password", key="PAT")
+  mode = st.radio("**Select the classification mode**",["ICL zero shot","Few shot"] )
+  
+  if mode == "Few shot":
+    no_of_examples = st.number_input("Enter the number of examples",min_value=1,max_value=25)
+        
+  zst = zero_shot_examples()
+  if PAT and mode:
+    if "config" not in st.session_state.keys():
+      st.session_state["config"] = True
+
 try:
-  if st.session_state['start_chat'] and st.session_state["config"] and st.session_state['few_shot_examples']:
-    textbox()
+  lm = model_Select()
+  if st.session_state['start_chat'] and st.session_state["config"]:
+    if mode == "Few shot":
+      textbox(lm, mode, zst, no_of_examples)
+    else:
+      textbox(lm, mode, zst)
+      
     st.markdown(
         "<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>",
         unsafe_allow_html=True,
     )
 except Exception as e:
-  st.error(f"""Classification failed due to {e},\n\n1. Make sure you have uploaded the data\n2. Make sure you have entered the Clarifai PAT\n3. Make sure you have entered the Clarifai LLM model URL""")
+  st.error(f"""Classification failed due to {e}.""")
--- utils.py
+++ utils.py
@@ -1,35 +1,58 @@
 import os
 import tempfile
-from langchain.prompts.few_shot import FewShotPromptTemplate
-from langchain.prompts.prompt import PromptTemplate
-from unstructured.partition.pdf import partition_pdf
-
-def process_pdf(pdf_file):
-    temp_dir = tempfile.TemporaryDirectory()
-    for file in pdf_file:
-        temp_filepath = os.path.join(temp_dir.name, file.name)
-        with open(temp_filepath, "wb") as f:
-            f.write(file.getvalue())
-            
-    elements = partition_pdf(filename=temp_filepath,
-                         infer_table_structure=True,
-                         strategy='hi_res',
-    )
-    return [elem.metadata.text_as_html for elem in elements if elem.category == "Table"]
 
 def prompt_template(classify_examples, user_query):
+    prompt = f"""Human: Your job is to use information from the following classification guide to classify the input text.
+
+    classification guide : {{{classify_examples}}}
+
+
+    Compate the input text to the first column of the classification guide table to find the most relevant row.
+    Your response should be limited to:
+    "Classification: {{level\}}//{{dissem control}}, Derivative: {{derivative}}, Reason: {{reason}} De-classify on: {{declass info}}
+
+    Justification: item
+    Remarks: remarks", where level, dissem control, derivative, reason, declass info, and item are copied verbatium from the row in the classification guide that most relates to the input text. Note that "item" is found in the first column of a table. Note that in many cases dissem control, derivative, declass info, is missing: in that case do not respond with that information. You must only use information contained in the classification guide.
+    In cases where the information in classification guide is not sufficient to confidently classify the input text, you must respond with "Unable to determine classification using the provided classification guide."
+
+    Real human input
+    input text : {{{user_query}}}
+    Assistant:
+    """
+    return prompt
+
+def few_shot_examples(vectorDB, user_query):
     
-    return (f"""Human: You are highly qualified personnel who has been working as a analyst in secret service for government. You have been assigned to classify the documents based on the content. You have given a knowledge base which contains the information about\n
-    Various government related documents.\n
-    classify_examples : {{{classify_examples}}}\n
-    user_query : {{{user_query}}}\n
-    Follow the above `classify_examples` examples which was classified by you previously and use as reference to classify the `user_query` documents. Go through different examples for classification labels and respond with only \n
-    classification label and rationale justification for your classification\n Assistant:""")
+  resp = vectorDB.similarity_search(user_query)
+  return list(set([doc.page_content for doc in resp]))
+
+def few_shot_promt_template(few_shot_examples,user_query):
+    
+  prompt=f"""Human: Your job is to use information from the following classification guide to classify the input text.\n
+  The classification guide consists of few shot examples that you can refer to answer the user's query.\n
+  The few shot example is made of list with each field in the list corresponds to specific column :\n
+  schema : ['Item','Level','Derivative','Dissemination Control','Reason','Declassify','Remarks'] \n
+  few_shot_examples : {few_shot_examples},
+
+  Your response should be limited to:
+  "Classification: {{level\}}//{{dissem control}}, Derivative: {{derivative}}, Reason: {{reason}} De-classify on: {{declass info}}
+
+  Justification: item
+  Remarks: remarks", where level, dissem control, derivative, reason, declass info, and item are copied verbatium from the row in the classification guide that most relates to the input text. Note that "item" is found in the first column of a table. Note that in many cases dissem control, derivative, declass info, is missing: in that case do not respond with that information. You must only use information contained in the classification guide.
+  In cases where the information in classification guide is not sufficient to confidently classify the input text, you must respond with "Unable to determine classification using the provided classification guide."
+
+  Real human input
+  input text : {{{user_query}}}
+  Assistant:
+
+  """
+  return prompt
 
 def llm_models():
     model_list=["openAI:GPT-4","llama2-70B-chat","openAI:GPT-4-turbo","mistral-7B-Instruct",
                 "Anthropic:claude-v2","Anthropic:claude-2.1","Anthropic:claude-v1","gcp:gemini-pro",
-                "Anthropic:claude-instant-1.2","mistral-large","databricks:dbrx-intruct"]
+                "Anthropic:claude-instant-1.2","mistral-large","mistral-small",
+                "mistral-medium","databricks:dbrx-intruct", "Upstage:solar-10 7b-instruct"]
     
     model_params={"openAI:GPT-4":"https://clarifai.com/openai/chat-completion/models/GPT-4",
                   "llama2-70B-chat":"https://clarifai.com/meta/Llama-2/models/llama2-70b-chat",
@@ -38,11 +61,15 @@ def llm_models():
                   "Anthropic:claude-v2":"https://clarifai.com/anthropic/completion/models/claude-v2",
                   "Anthropic:claude-2.1":"https://clarifai.com/anthropic/completion/models/claude-2_1",
                   "Anthropic:claude-v1":"https://clarifai.com/anthropic/completion/models/claude-v1",
+                  "Upstage:solar-10 7b-instruct":"https://clarifai.com/upstage/solar/models/solar-10_7b-instruct",
                   "gcp:gemini-pro":"https://clarifai.com/gcp/generate/models/gemini-pro",
                   "Anthropic:claude-instant-1.2":"https://clarifai.com/anthropic/completion/models/claude-instant-1_2",
                   "mistral-large":"https://clarifai.com/mistralai/completion/models/mistral-large",
+                  "mistral-small":"https://clarifai.com/mistralai/completion/models/mistral-small",
+                  "mistral-medium":"https://clarifai.com/mistralai/completion/models/mistral-medium",
                   "databricks:dbrx-intruct":"https://clarifai.com/databricks/drbx/models/dbrx-instruct",
                   }
+    
     return model_list, model_params
 
     
